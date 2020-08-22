fb59adcb6c85359592eadc6758b7d4973cae1e3a
Luciana Santoro <santoro_luciana@hotmail.com>
2020-08-22 16:37:30 -0300

    Repetition cycle exercise one version A

--- src/ar/com/ar/online/second/repetitioncycles/Main.java
+++ src/ar/com/ar/online/second/repetitioncycles/Main.java
@@ -3,6 +3,10 @@ package ar.com.ar.online.second.repetitioncycles;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Se requiere obtener la suma de las cantidades contenidas en un arreglo de 10 elementos.
+
+
 	// write your code here
     }
 }
